a3e4077521c5935b378e06b8cf54759e07b5e97e
Harry Prior <harryprior@hotmail.co.uk>
2020-11-02 14:21:09 +0000

    added class library with GitHub scrapers

---
+++ PortfolioBlogLibrary/GitScraper/GitHubScraper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioBlogLibrary.GitScraper
+{
+    class GitHubScraper : IGitHubScraper
+    {
+        IList<string> GetReadMeStrings()
+        {
+            return "";
+        }
+    }
+}
