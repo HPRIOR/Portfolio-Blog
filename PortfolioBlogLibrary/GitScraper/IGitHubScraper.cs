f06554b84c8607a3fc61aee44dc84bb82b110629
Harry Prior <harryprior@hotmail.co.uk>
2020-11-02 14:43:30 +0000

    Add interface - IGitHubScraper.cs

--- PortfolioBlogLibrary/GitScraper/IGitHubScraper.cs
+++ PortfolioBlogLibrary/GitScraper/IGitHubScraper.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PortfolioBlogLibrary.GitScraper
 {
-    interface IGitHubScraper
+    public interface IGitHubScraper
     {
         IList<string> GetReadMeStrings();
     }
