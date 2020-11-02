24b197857cee89fd7cacbfa79ae545573e5cd71d
Harry Prior <harryprior@hotmail.co.uk>
2020-11-02 14:50:47 +0000

    Rename GitHubScraperTests

--- PortfolioBlogLibraryTests/UnitTest1.cs
+++ PortfolioBlogLibraryTests/UnitTest1.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace PortfolioBlogLibraryTests
 {
-    public class Tests
+    public class GitHubScraperTests
     {
         [SetUp]
         public void Setup()
