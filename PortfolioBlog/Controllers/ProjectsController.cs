350c443c3f8b7c93c96fedabd1d159fd4fbdf4f7
Harry Prior <harryprior@hotmail.co.uk>
2020-11-02 14:24:50 +0000

    Delete code in controller related to html helper class

--- PortfolioBlog/Controllers/ProjectsController.cs
+++ PortfolioBlog/Controllers/ProjectsController.cs
@@ -12,31 +12,15 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace PortfolioBlog.Controllers
 {
-    internal class JsonTitle
-    {
-        public string title { get; set; }
-    }
     public class ProjectsController : Controller
     {
 
-        public string GetUrl(string title)
-            =>  $"https://github.com/HPRIOR/{title}/blob/master/README.md";
         
-        private IList<string> GetTitles()
-        {
-            string jsonString = System.IO.File.ReadAllText(@"..\Json\git-titles.json");
-            JObject jObject = JObject.Parse(jsonString);
-            JArray jArray = (JArray)jObject["projects"];
-            return jArray.ToObject<IList<JsonTitle>>().Select(x => x.title).ToList();             
-        }
             
         public IActionResult Index()
         {
 
-            var readMe = HtmlHelper.GetHtmlSection( "https://github.com/HPRIOR/PHP-XML-Translation/blob/master/README.md", "//*[@id='readme']/article");
-            var projectModel = new Project();
-            projectModel.ReadMe = readMe;
-            return View(projectModel);
+            return View();
         }
 
         public IActionResult ProjectReadMe()
