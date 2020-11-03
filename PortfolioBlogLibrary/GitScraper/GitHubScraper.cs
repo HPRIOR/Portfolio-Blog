a1e7c780e0fb9f9895a4589d8865464202581f3f
Harry Prior <harryprior@hotmail.co.uk>
2020-11-03 10:28:39 +0000

    Add methods for scraping github readme projects

--- PortfolioBlogLibrary/GitScraper/GitHubScraper.cs
+++ PortfolioBlogLibrary/GitScraper/GitHubScraper.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace PortfolioBlogLibrary.GitScraper
 {
     public class GitHubScraper : IGitHubScraper
     {
-
-        public IList<string> GetReadMeStrings()
+        private HttpClient _httpClient;
+     
+        public GitHubScraper(HttpClient httpClient)
         {
-            return new List<string>() { "Hello world" };
+            _httpClient = httpClient;
+
         }
+
+        public string GetUserUrl(string userName) => "https://github.com" + "/" + userName + "?tab=repositories";
+        public string GetReadmeUrl(string repoName) => "https://github.com" +  repoName + "/blob/master/README.md";
+
+        public IList<string> ProcessHtmlToList(string html, string xpath, Func<HtmlNode, string> whichNodes)
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            return htmlDocument
+                .DocumentNode
+                .SelectNodes(xpath)
+                .Select(whichNodes)
+                .ToList();
+        }
+
+        public string ProcessHtmlToString(string html, string xpath)
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            return htmlDocument
+                .DocumentNode
+                .SelectSingleNode(xpath)
+                .OuterHtml;        
+        }
+       
+        public async Task<string> GetHtmlString(string url)
+        {
+            try
+            {
+                return await _httpClient.GetStringAsync(url);
+               
+            }
+            catch(HttpRequestException e)
+            {
+                throw e;
+            }
+                                   
+        }
+
+
+     
     }
 }
+
--- PortfolioBlogLibrary/GitScraper/IGitHubScraper.cs
+++ PortfolioBlogLibrary/GitScraper/IGitHubScraper.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PortfolioBlogLibrary.GitScraper
 {
     public interface IGitHubScraper
-    {
-        IList<string> GetReadMeStrings();
+    {   
+        Task<string> GetHtmlString(string href);
+        public IList<string> ProcessHtmlToList(string html, string xpath, Func<HtmlNode, string> whichNodes);
+        string GetUserUrl(string user);
+        string GetReadmeUrl(string repoName);
+        string ProcessHtmlToString(string html, string xpath);
+
+
+
     }
 }
 
