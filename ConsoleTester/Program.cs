fbc46002afecb1e990cfa45ae748f40785e124ab
Harry Prior <harryprior@hotmail.co.uk>
2020-11-03 11:08:05 +0000

    Test scraper in console app

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 using PortfolioBlogLibrary.GitScraper;
 
 namespace ConsoleTester
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using var client = new HttpClient();
+            GitHubScraper gitHubScraper = new GitHubScraper(client);
+            var result = await gitHubScraper.GetHtmlString(gitHubScraper.GetUserUrl("HPRIOR"));
+            var repoList = gitHubScraper.ProcessHtmlToList(result, "//*[@id='user-repositories-list']/ul/li/div/div/h3/a",  node => node.GetAttributeValue("href", "default"));
+            /*repoList.ToList().ForEach(async repo =>
+            {
+                var repoURL = gitHubScraper.GetReadmeUrl(repo);
+                Console.WriteLine(repoURL);
+                var repoHtml = await gitHubScraper.GetHtmlString(repoURL);
+                Console.Write(repoHtml);
+                Console.WriteLine(gitHubScraper.ProcessHtmlToString(repoHtml, "//*[@id='readme']"));
+            });*/
+            try
+            {
+                var repoUrl = gitHubScraper.GetReadmeUrl(repoList[2]);
+                Console.WriteLine(repoUrl);
+                var repoHtml = await gitHubScraper.GetHtmlString(repoUrl);
+                var readme = gitHubScraper.ProcessHtmlToString(repoHtml, "//*[@id='readme']");
+                Console.WriteLine(readme);
+
+            }
+            catch(HttpRequestException)
+            {
+                Console.WriteLine("Uh OH");
+            }
+            
         }
+
     }
 }
