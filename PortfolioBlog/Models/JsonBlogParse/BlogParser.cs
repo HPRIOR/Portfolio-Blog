a4211041fe07c9bf63236debc6b6b5311a078d8a
Harry Prior <harryprior@hotmail.co.uk>
2020-11-06 13:20:55 +0000

    Add class to parse json to blog post

---
+++ PortfolioBlog/Models/JsonBlogParse/BlogParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace PortfolioBlog.Models.JsonBlogParse
+{
+    public class BlogParser : IBlogParser
+    {
+        public Blog GetBlog(string json) 
+            => JsonConvert.DeserializeObject<Blog>(json);
+
+    }
+}
+
---
+++ PortfolioBlog/Models/JsonBlogParse/IBlogParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace PortfolioBlog.Models.JsonBlogParse
+{
+    public interface IBlogParser
+    {
+        Blog GetBlog(string json);
+
+    }
+}
