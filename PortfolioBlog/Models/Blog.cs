5c4a23ac55ad5c84898fc71a4597f375ff98ea79
Harry Prior <harryprior@hotmail.co.uk>
2020-11-06 13:20:29 +0000

    Add datamodel for Blog post

---
+++ PortfolioBlog/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PortfolioBlogLibrary.JsonBlogParse;
+
+namespace PortfolioBlog.Models
+{
+    public class Blog : IBlog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
