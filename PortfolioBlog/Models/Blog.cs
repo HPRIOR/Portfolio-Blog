40aa7293bc9f585ce2ca93d26d04e01036c214a4
Harry Prior <harryprior@hotmail.co.uk>
2020-11-07 09:18:41 +0000

    Remove interface from Blog model

--- PortfolioBlog/Models/Blog.cs
+++ PortfolioBlog/Models/Blog.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using PortfolioBlogLibrary.JsonBlogParse;
+
 
 namespace PortfolioBlog.Models
 {
-    public class Blog : IBlog
+    public class Blog 
     {
         public int Id { get; set; }
         public string Title { get; set; }
